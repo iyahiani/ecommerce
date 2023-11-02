a38c18da82db4c6877e731ccdc1268d33c843871
yahianiismail <Hawma@122020>
2023-11-02 21:11:20 +0100

    correction login / Out

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from "../../environnements/environment";
+
+const AUTH_API = environment.api;
+const httpParams = new HttpParams();
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private httpOption = new HttpParams();
+  constructor(private http: HttpClient) {
+  }
+
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + '/login',
+      {
+        email,
+        password,
+      }
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'signout', { }, httpOptions);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StorageService } from './_services/storage.service';
+import { AuthService } from './_services/auth.service';
+import { EventBusService } from './_shared/event-bus.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecom';
+  private roles: string[] = ['ROLE_ADMIN',''];
+  isLoggedIn = true;
+  showAdminBoard = true;
+  showModeratorBoard = true;
+  username?: string;
+
+  eventBusSub?: Subscription;
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService,
+    private eventBusService: EventBusService
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.logout();
+    });
+  }
+
+  logout(): void {
+    this.storageService.clean();
+    window.location.reload();
+  }
 }
